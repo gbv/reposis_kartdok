3af7ee50946d79dda57b2ff1d46b40a64b6b782b
Andre Herzog <andre.herzog@gbv.de>
2021-02-18 12:13:34 +0100

    REP-666 Added typeahead for the kartdok classification field.

--- save/webpages/js/project.js
+++ save/webpages/js/project.js
@@ -35,8 +35,17 @@ $(document).ready(function() {
     toggleOAOptions();
   });
 
+  $(".bc-select").each(function () {
+    // setDefault($(this));
+     if ($(this).children("option").length > 0) {
+         setLabelForClassificationBC($(this));
+     }
+     else {
+         setSelect2BC($(this));
+     }
+  });
+  
 });
-
 
 function toggleOAOptions() {
   var duration = 500;
@@ -52,3 +61,96 @@ function toggleOAOptions() {
     localStorage.setItem("open_aire_options_are_visible", true);
   }
 }
+
+// TODO: Parameterize the select function in MIR (type-ahead)
+function setLabelForClassificationBC(parent) {
+  $.ajax({
+      url: webApplicationBaseURL + 'servlets/solr/select',
+      data: {
+              q: optionsToQuery(parent),
+              fq: 'classification:kartdok_bc',
+              wt: 'json',
+              core: 'classification'
+      },
+      dataType: 'json'
+  }).done(function(data) {
+      $.each(data.response.docs, function (_i, cat) {
+          let text = cat['label.' + $("html").attr("lang")][0];
+          if (text === undefined) {
+              text = cat['label.en'][0]
+          }
+          getOptionWithValBC(parent, cat.category).html(text);
+      });
+      setSelect2BC(parent);
+  });
+}
+
+function optionsToQuery(elm) {
+  let query = [];
+  $(elm).children().each(function (i, option) {
+      if ($(option).val() !== "") {
+          query.push('category:' + $(option).val());
+      }
+  });
+  return query.join(" OR ");
+}
+
+function setSelect2BC(elm) {
+  $(elm).select2({
+      ajax: {
+          url: webApplicationBaseURL + 'servlets/solr/select',
+          data: function (params) {
+              params.term = (params.term == null) ? "" : params.term;
+              return {
+                  q: '-id:kartdok_bc OR category *' + params.term.replace(/\./g, "_") + "* OR " + 'label.en *' + params.term + "* OR " + 'label.de *' + params.term + "*",
+                  fq: 'classification:kartdok_bc',
+                  rows: 2147483647,
+                  sort: 'category ASC',
+                  wt: 'json',
+                  core: 'classification'
+              };
+          },
+          dataType: 'json',
+          processResults: function (data) {
+              let res = {
+                  results: $.map(data.response.docs, function(obj) {
+                      let text = obj['label.' + $("html").attr("lang")];
+                      if (text === undefined) {
+                          text = obj['label.en'][0]
+                      }
+                      else {
+                          text = text[0];
+                      }
+                      return { id: obj.category, text: text };
+                  })
+              };
+              addDefault(elm, res);
+              return res;
+          },
+      },
+      minimumInputLength: 0,
+      language: $("html").attr("lang")
+  });
+}
+
+function addDefault(elm, res) {
+  $(elm).children().each(function (i, option) {
+      let found = false;
+      $.each(res.results, function (i, solrOption) {
+          if (solrOption.id === $(option).val()) {
+              found = true;
+              return false;
+          }
+      });
+      if (!found) {
+          if ($(option).val() !== "") {
+              res.results.push({id: $(option).val(), text: $(option).html()})
+          }
+      }
+  })
+}
+
+function getOptionWithValBC(elm, val) {
+  return $(elm).find("option[value='" + val + "']");
+}
+
