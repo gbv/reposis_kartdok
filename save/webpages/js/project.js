f6732152a9877378c4e5682b555dc69f176a5cb0
Kathleen Neumann <neumann@gbv.de>
2021-10-04 11:02:19 +0200

    add all href variants to replace script

--- save/webpages/js/project.js
+++ save/webpages/js/project.js
@@ -48,12 +48,12 @@ $(document).ready(function() {
 
   // replace placeholder USERNAME with username
   var userID = $("#currentUser strong").html();
-  var newHref = 'http://localhost:18041/kartdok/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
-  $("a[href='http://localhost:18041/kartdok/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHref);
-//  var newHref = 'https://reposis-test.gbv.de/kartdok/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
-//  $("a[href='https://reposis-test.gbv.de/kartdok/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHref);
-//  var newHref = 'https://kartdok.staatsbibliothek-berlin.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
-//  $("a[href='https://kartdok.staatsbibliothek-berlin.de/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHref);
+  var localHref = 'http://localhost:18041/kartdok/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='http://localhost:18041/kartdok/servlets/solr/select?q=createdby:USERNAME']").attr('href', localHref);
+  var testHref = 'https://reposis-test.gbv.de/kartdok/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='https://reposis-test.gbv.de/kartdok/servlets/solr/select?q=createdby:USERNAME']").attr('href', testHref);
+  var prodHref = 'https://kartdok.staatsbibliothek-berlin.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='https://kartdok.staatsbibliothek-berlin.de/servlets/solr/select?q=createdby:USERNAME']").attr('href', prodHref);
 
 
 
